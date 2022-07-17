acc1d5b9bfc58ad03193dd8be6a7bf3b99834c43
vivianbh <hadallir@gmail.com>
2022-07-18 01:39:21 +0800

    devel dirctory

--- src/robot_localization/src/apriltag_coordinate_transformation.cpp
+++ src/robot_localization/src/apriltag_coordinate_transformation.cpp
@@ -3,10 +3,11 @@
 #include <unistd.h>
 #include <iostream>
 #include <ros/ros.h>
-#include <Eigen/Core>
+//#include <Eigen/Core>
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 #include <vector>
+#include <map>
 #include "std_msgs/Header.h"
 #include "geometry_msgs/Pose.h"
 #include "apriltag_ros/AprilTagDetectionArray.h"
@@ -24,22 +25,27 @@ geometry_msgs::PoseWithCovarianceStamped Calibration_f2;
 geometry_msgs::PoseWithCovarianceStamped Calibration_d3;
 geometry_msgs::PoseWithCovarianceStamped Calibration_f3;
 
-std_msgs::Header header_d;
-std_msgs::Header header_f;
+vector<std_msgs::Header> header_d;
+vector<std_msgs::Header> header_f;
 
 float odomX, odomY, odomZ;
-//float d_x_err, d_y_err, d_z_err;
-vector<vector<float>> downward_pos_error;
-vector<vector<float>> downward_pos;
-float f_x_err, f_y_err, f_z_err;
-float calf_x, calf_y, calf_z;
-
-vector<Eigen::Quaterniond> q_d;
-Eigen::Quaterniond q_f;
- 
-float x_f, y_f, z_f, w_f, i_f, j_f, k_f;
 vector<vector<float>> forward_tags(3, vector<float>(7));  // (x, y, z, w, i, j, k)
 vector<vector<float>> downward_tags(3, vector<float>(7));
+
+// centre ids of tag_bundles and each position
+map<int, vector<float>> downward_id = {
+	{0, {0, 0, 0}},
+	{10, {0, 2, 0}},
+	{20, {0, -2, 0}}
+};
+
+map<int, vector<float>> forward_id = {
+	{5, {4.01, 0, 2}},
+	{15, {4.01, 2, 2}},
+	{25, {4.01, -2, 2}}
+};
+
+//Matrix<float, 3, 3> forwardTag2world;
 
 void OdeCallback(const geometry_msgs::Pose::ConstPtr &msg)
 {
@@ -50,12 +56,15 @@ void OdeCallback(const geometry_msgs::Pose::ConstPtr &msg)
 
 }
 
-void IrisPositionDownword(vector<Eigen::Quaterniond> q_d)
+void IrisPositionDownword(vector<vector<float>> &q_d, vector<int> &detected_id)
 {
 	vector<float> temp_pos;
+	vector<vector<float>> downward_pos;
+	vector<vector<float>> downward_pos_error;
 
 	for(unsigned int num = 0; num < q_d.size(); num++) {
-		temp_pos = { float(q_d[num].x() + 0), float(q_d[num].y() + 0), float(q_d[num].z() + 0.01 + 0.05) };
+		q_d[num][2] += (0.01 + 0.05);  // calibration to tag frame
+		temp_pos = { q_d[num][0], q_d[num][1], q_d[num][2] };
 		downward_pos.push_back(temp_pos);
 	}
 
@@ -63,36 +72,36 @@ void IrisPositionDownword(vector<Eigen::Quaterniond> q_d)
 		switch(num) {
 			case 0:    // first downward pose sensor
 				// Message of downward camera
-				Calibration_d1.header.stamp = header_d.stamp;
+				Calibration_d1.header = header_d[num];
 				Calibration_d1.header.frame_id = "iris1/odometry_sensor1";
 
-				Calibration_d1.pose.pose.position.x = downward_pos[num][0];
-				Calibration_d1.pose.pose.position.y = downward_pos[num][1];
-				Calibration_d1.pose.pose.position.z = downward_pos[num][2];
+				Calibration_d1.pose.pose.position.x = downward_pos[num][0] + downward_id[detected_id[num]].at(0);
+				Calibration_d1.pose.pose.position.y = downward_pos[num][1] + downward_id[detected_id[num]].at(1);
+				Calibration_d1.pose.pose.position.z = downward_pos[num][2] + downward_id[detected_id[num]].at(2);
 
 				downward_pos_error.push_back( { abs(odomX-downward_pos[num][0]), abs(odomY-downward_pos[num][1]), abs(odomZ-downward_pos[num][2]) } );
 
 				break;
 			case 1:   // second downward pose sensor
 				// Message of downward camera
-				Calibration_d2.header.stamp = header_d.stamp;
+				Calibration_d2.header = header_d[num];
 				Calibration_d2.header.frame_id = "iris1/odometry_sensor1";
 
-				Calibration_d2.pose.pose.position.x = downward_pos[num][0];
-				Calibration_d2.pose.pose.position.y = downward_pos[num][1];
-				Calibration_d2.pose.pose.position.z = downward_pos[num][2];
+				Calibration_d2.pose.pose.position.x = downward_pos[num][0] + downward_id[detected_id[num]].at(0);
+				Calibration_d2.pose.pose.position.y = downward_pos[num][1] + downward_id[detected_id[num]].at(1);
+				Calibration_d2.pose.pose.position.z = downward_pos[num][2] + downward_id[detected_id[num]].at(2);
 
 				downward_pos_error.push_back( { abs(odomX-downward_pos[num][0]), abs(odomY-downward_pos[num][1]), abs(odomZ-downward_pos[num][2]) } );
 
 				break;
 			case 2:   // third downward pose sensor
 				// Message of downward camera
-				Calibration_d3.header.stamp = header_d.stamp;
+				Calibration_d3.header = header_d[num];
 				Calibration_d3.header.frame_id = "iris1/odometry_sensor1";
 
-				Calibration_d3.pose.pose.position.x = downward_pos[num][0];
-				Calibration_d3.pose.pose.position.y = downward_pos[num][1];
-				Calibration_d3.pose.pose.position.z = downward_pos[num][2];
+				Calibration_d3.pose.pose.position.x = downward_pos[num][0] + downward_id[detected_id[num]].at(0);
+				Calibration_d3.pose.pose.position.y = downward_pos[num][1] + downward_id[detected_id[num]].at(1);
+				Calibration_d3.pose.pose.position.z = downward_pos[num][2] + downward_id[detected_id[num]].at(2);
 
 				downward_pos_error.push_back( { abs(odomX-downward_pos[num][0]), abs(odomY-downward_pos[num][1]), abs(odomZ-downward_pos[num][2]) } );
 
@@ -104,29 +113,72 @@ void IrisPositionDownword(vector<Eigen::Quaterniond> q_d)
 	
 }
 
-void Print_forward_error(float x, float y, float z)
+void IrisPositionForword(vector<vector<float>> &q_f, vector<int> &detected_id)
 {
-	calf_x = 3 - (z + 0.01) - 0.1;
-	calf_y = y;
-	calf_z = 3 + x;
+	vector<float> temp_pos;
+	//Matrix<float, 3, 1> rota_pos; // = Matrix3f::Zero();
+	//Matrix<float, 1, 3> temp_pos;
+	vector<vector<float>> forward_pos_error;
+	vector<vector<float>> forward_pos;
 
-	// Message of forward camera
-	Calibration_f1.header.stamp = header_f.stamp;
-	Calibration_f1.header.frame_id = "iris1/odometry_sensor1";
-	Calibration_f1.pose.pose.position.x = calf_x;
-	Calibration_f1.pose.pose.position.y = calf_y;
-	Calibration_f1.pose.pose.position.z = calf_z;
+	//forwardTag2world << 0, 0, 1, 0, 1, 0, -1, 0, 0;
 
-	//cout << "calbration_f_x: " << calf_x << ", calbration_f_y: " << calf_y << ", calbration_f_z: " << calf_z << endl;
+	for(unsigned int num = 0; num < q_f.size(); num++) {
+		q_f[num][2] += (0.01 + 0.1);  // calibration to tag frame
+		temp_pos = {q_f[num][0], q_f[num][1], q_f[num][2]};
+		//rota_pos = forwardTag2world * temp_pos.transpose();
 
-	f_x_err = abs(odomX - calf_x);
-	f_y_err = abs(odomY - calf_y);
-	f_z_err = abs(odomZ - calf_z);
+		forward_pos.push_back(temp_pos);
+		//forward_pos.push_back( {rota_pos.coeff(0, 0), rota_pos.coeff(1, 0), rota_pos.coeff(2, 0)} );
+	}
 
-	//cout << "forward_error_x: " << f_x_err << ", forward_error_y: " << f_y_err 
-	//	<< ", forward_error_z: " << f_z_err << endl << endl;
+	cout << "Finished to transfer.    q_f size: " << q_f.size() << endl; 
+
+	for(unsigned int num = 0; num < q_f.size(); num++) {
+		cout << "num: " << num << endl;
+		switch(num) {
+			case 0:    // first forward pose sensor
+				// Message of forward camera
+				Calibration_f1.header = header_f[num];
+				Calibration_f1.header.frame_id = "iris1/odometry_sensor1";
+
+				Calibration_f1.pose.pose.position.x = - forward_pos[num][2] + forward_id[detected_id[num]].at(0);
+				Calibration_f1.pose.pose.position.y = forward_pos[num][1] + forward_id[detected_id[num]].at(1);
+				Calibration_f1.pose.pose.position.z = forward_pos[num][0] + forward_id[detected_id[num]].at(2);
+
+				forward_pos_error.push_back( { abs(odomX-forward_pos[num][0]), abs(odomY-forward_pos[num][1]), abs(odomZ-forward_pos[num][2]) } );
+
+				break;
+			case 1:   // second forward pose sensor
+				// Message of forward camera
+				Calibration_f2.header = header_f[num];
+				Calibration_f2.header.frame_id = "iris1/odometry_sensor1";
+
+				Calibration_f2.pose.pose.position.x = - forward_pos[num][2] + forward_id[detected_id[num]].at(0);
+				Calibration_f2.pose.pose.position.y = forward_pos[num][1] + forward_id[detected_id[num]].at(1);
+				Calibration_f2.pose.pose.position.z = forward_pos[num][0] + forward_id[detected_id[num]].at(2);
+
+				forward_pos_error.push_back( { abs(odomX-forward_pos[num][0]), abs(odomY-forward_pos[num][1]), abs(odomZ-forward_pos[num][2]) } );
+
+				break;
+			case 2:   // third forward pose sensor
+				// Message of forward camera
+				Calibration_f3.header = header_f[num];
+				Calibration_f3.header.frame_id = "iris1/odometry_sensor1";
+
+				Calibration_f3.pose.pose.position.x = - forward_pos[num][2] + forward_id[detected_id[num]].at(0);
+				Calibration_f3.pose.pose.position.y = forward_pos[num][1] + forward_id[detected_id[num]].at(1);
+				Calibration_f3.pose.pose.position.z = forward_pos[num][0] + forward_id[detected_id[num]].at(2);
+
+				forward_pos_error.push_back( { abs(odomX-forward_pos[num][0]), abs(odomY-forward_pos[num][1]), abs(odomZ-forward_pos[num][2]) } );
+
+				break;
+			default:
+				break;
+		}
+	}
 }
-
+/*
 Eigen::Quaterniond Quaterion_calcutaion(float i, float j, float k, float w, float x, float y, float z)
 {
 	Eigen::Quaterniond q1;
@@ -140,48 +192,68 @@ Eigen::Quaterniond Quaterion_calcutaion(float i, float j, float k, float w, floa
 
 	return q3;
 }
-
+*/
 void downCallback(const apriltag_ros::AprilTagDetectionArray::ConstPtr &msg)
 {
 
-	Eigen::Quaterniond temp_q;
+	vector<float> temp_q;
+	vector<int> detected_id;
+	vector<vector<float>> q_d;
 	
 	if(msg->detections.size() > 0) {
-		header_d = msg->detections[0].pose.header;
 
 		for(unsigned int count = 0; count < msg->detections.size(); count++) {
-			downward_tags[count][0] = msg->detections[count].pose.pose.pose.position.x;
-			downward_tags[count][1] = msg->detections[count].pose.pose.pose.position.y;
+			detected_id.push_back(msg->detections[count].id[0]);
+
+			header_d.push_back( msg->detections[count].pose.header );
+
+			downward_tags[count][1] = msg->detections[count].pose.pose.pose.position.x;
+			downward_tags[count][0] = msg->detections[count].pose.pose.pose.position.y;
 			downward_tags[count][2] = msg->detections[count].pose.pose.pose.position.z;
 			downward_tags[count][3] = msg->detections[count].pose.pose.pose.orientation.w;
 			downward_tags[count][4] = msg->detections[count].pose.pose.pose.orientation.x;
 			downward_tags[count][5] = msg->detections[count].pose.pose.pose.orientation.y;
 			downward_tags[count][6] = msg->detections[count].pose.pose.pose.orientation.z;
 
-			temp_q = Quaterion_calcutaion(downward_tags[count][4], downward_tags[count][5], downward_tags[count][6], downward_tags[count][3], downward_tags[count][0], downward_tags[count][1], downward_tags[count][2]);
+			temp_q = {downward_tags[count][0], downward_tags[count][1], downward_tags[count][2]};
 			q_d.push_back(temp_q);
 		}
 		
-		IrisPositionDownword(q_d);
+		IrisPositionDownword(q_d, detected_id);
+		header_d.clear();
 	}
 }
 
 void forCallback(const apriltag_ros::AprilTagDetectionArray::ConstPtr &msg)
 {
+	vector<float> temp_q;
+	vector<int> detected_id;
+	vector<vector<float>> q_f;
+
+	cout << "Forward camera: " << msg->detections.size() << " poses detected" << endl;
+	
 	if(msg->detections.size() > 0) {
-		header_f = msg->detections[0].pose.header;
-		x_f = msg->detections[0].pose.pose.pose.position.x;
-		y_f = msg->detections[0].pose.pose.pose.position.y;
-		z_f = msg->detections[0].pose.pose.pose.position.z;
-		i_f = msg->detections[0].pose.pose.pose.orientation.x;
-		j_f = msg->detections[0].pose.pose.pose.orientation.y;
-		k_f = msg->detections[0].pose.pose.pose.orientation.z;
-		w_f = msg->detections[0].pose.pose.pose.orientation.w;
 
-		q_f = Quaterion_calcutaion(i_f, j_f, k_f, w_f, x_f, y_f, z_f);
+		for(unsigned int count = 0; count < msg->detections.size(); count++) {
+			detected_id.push_back(msg->detections[count].id[0]);
 
-		Print_forward_error(-q_f.x(), -q_f.y(), -q_f.z());
-	} 
+			header_f.push_back( msg->detections[count].pose.header );
+
+			forward_tags[count][1] = msg->detections[count].pose.pose.pose.position.x;
+			forward_tags[count][0] = msg->detections[count].pose.pose.pose.position.y;
+			forward_tags[count][2] = msg->detections[count].pose.pose.pose.position.z;
+			forward_tags[count][3] = msg->detections[count].pose.pose.pose.orientation.w;
+			forward_tags[count][4] = msg->detections[count].pose.pose.pose.orientation.x;
+			forward_tags[count][5] = msg->detections[count].pose.pose.pose.orientation.y;
+			forward_tags[count][6] = msg->detections[count].pose.pose.pose.orientation.z;
+
+			temp_q = {forward_tags[count][0], forward_tags[count][1], forward_tags[count][2]};
+			q_f.push_back(temp_q);
+		}
+		
+		IrisPositionForword(q_f, detected_id);
+		header_f.clear();
+	}
 }
 
 int main(int argc, char** argv)
@@ -218,6 +290,7 @@ int main(int argc, char** argv)
 
 	while (ros::ok())
 	{
+		
 		Pose_detect_downward1.publish(Calibration_d1);
 		Pose_detect_forward1.publish(Calibration_f1);
 		Pose_detect_downward2.publish(Calibration_d2);
